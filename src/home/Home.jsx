79686149860479061def72ecc999f462f6950f17
ArpitChugh <arpitchugh.jbd.ac@gmail.com>
2022-04-24 22:09:55 +0530

    basic hero section completed, contact us remaining

--- src/home/Home.jsx
+++ src/home/Home.jsx
@@ -1,10 +1,37 @@
 import React from 'react';
-
+import { Link } from 'react-router-dom';
+import { FiGithub, FiInstagram, FiLinkedin } from 'react-icons/fi';
+import { AiOutlineMinus } from 'react-icons/ai';
+import './home.scss';
 const Home = () => {
 	return (
 		<>
 			{/* Hero Section */}
-			<section className='hero'>hi</section>
+			<section className='hero_wrapper'>
+				<div className='hero-contact'></div>
+				<div className='hero'>
+					<h1 className='hero-greeting'>
+						{/* <AiOutlineMinus /> */}
+						Hi
+					</h1>
+					<h3 className='hero-about'>
+						I’m <span className='hero-about__name'>Arpit</span>, 3rd Year
+						Computer Science Student, UI/UX Designer and Web Developer located
+						in Chandigarh, India, looking for working around the globe.
+					</h3>
+					<div className='hero-icons'>
+						<Link to='/'>
+							<FiInstagram />
+						</Link>
+						<Link to='/'>
+							<FiLinkedin />
+						</Link>
+						<Link to='/'>
+							<FiGithub />
+						</Link>
+					</div>
+				</div>
+			</section>
 		</>
 	);
 };
