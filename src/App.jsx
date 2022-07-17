ee7e210c054f6e70e73219c40e1e088cde914cdf
Arpit Chugh <arpitchugh.jbd.ac@gmail.com>
2022-07-17 15:57:06 +0530

    new loader added for modal

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,8 @@ import { Nav, HamNav } from './nav/Nav';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Cursor from './cursor/Cursor';
 
+import 'react-loader-spinner/dist/loader/css/react-spinner-loader.css';
+
 const App = () => {
 	return (
 		<Router>
--- src/modal/Modal.jsx
+++ src/modal/Modal.jsx
@@ -1,12 +1,16 @@
-import React from 'react';
+import { useState } from 'react';
 import './modal.scss';
+import { TailSpin } from 'react-loader-spinner';
 
 const Modal = ({ modal, setModal }) => {
+	const [loaded, setLoaded] = useState(false);
+
 	const clickHandler = e => {
 		if (e.target.classList.contains('modal-container')) {
 			setModal({ opened: false, title: '', link: '', src: '' });
 		}
 	};
+
 	return (
 		<div
 			className='modal-container'
@@ -30,9 +34,11 @@ const Modal = ({ modal, setModal }) => {
 						Link:<span> {modal.link}</span>
 					</a>
 					<div className='box'>
+						{!loaded && <TailSpin color='#00BFFF' height={80} width={80} />}
 						<img
+							onLoad={() => setLoaded(true)}
 							src={modal.src}
-							alt={modal.title}
+							alt={modal.title || 'loading-images'}
 							className='modal-container-elem-left-img'
 						/>
 					</div>
